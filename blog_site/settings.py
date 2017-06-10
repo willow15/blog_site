e3134919f8f0fa6102f67a16831d1b87b9a915fa
willow15 <willow15.liu@gmail.com>
2017-06-10 13:16:57 +0800

    update with MySQL settings

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	    'OPTIONS':{
+		    'read_default_file':'/home/willow15/Documents/blog_site/my.cnf',
+		    'init_command':'SET default_storage_engine=INNODB',
+		    'autocommit':True,
+	    },
     }
 }
 
